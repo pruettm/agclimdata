f58d343bfab58455598e5823b57292414291cd22
Matt Pruett <matthew.pruett@wsu.edu>
2021-10-18 09:15:48 -0700

    add raw code for loaction data

--- data-raw/grid_stats.R
+++ data-raw/grid_stats.R
@@ -1,3 +1,14 @@
 ## code to prepare `grid_stats` dataset goes here
+library(foreign)
+library(tidyverse)
+
+grid_stats <- read.dbf("data-raw/VICID_CO.DBF") |>
+  janitor::clean_names() |>
+  mutate(location = paste("data", vicclat, vicclon, sep = "_")) |>
+  select(location, lat = vicclat,
+         lon = vicclon, state, county, area_km2 = vic_km2) |>
+  mutate(across(where(is.factor), as.character)) |>
+  as_tibble()
+
 
 usethis::use_data(grid_stats, overwrite = TRUE)
